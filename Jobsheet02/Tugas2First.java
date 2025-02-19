7ec02f354fbb6bfd5c912fcb5b7917ef0b5b300e
Exc <excellchriatian12@gmail.com>
2025-02-19 13:47:22 +0700

    Added Tugas 2

--- Jobsheet02/Tugas2First.java
+++ Jobsheet02/Tugas2First.java
@@ -1,5 +1,15 @@
 package Jobsheet02;
 
 public class Tugas2First {
-    
+    public static void main(String[] args) {
+        Tugas2Zero student1 = new Tugas2Zero();
+        student1.NIM = "244107020227";
+        student1.Name = "Excell";
+        student1.className = "TI-1I";
+        student1.IPK = 3.8;
+        student1.printData();
+        student1.changeClass("TI-2I");
+        student1.updateIPK(4.0);
+        student1.printData(); 
+    }
 }

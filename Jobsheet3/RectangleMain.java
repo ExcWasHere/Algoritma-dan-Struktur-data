b9578d07924bf5b9836c651274df5523ede0d378
Exc <excellchriatian12@gmail.com>
2025-02-26 13:04:53 +0700

    Goes to Assignment

--- Jobsheet3/RectangleMain.java
+++ Jobsheet3/RectangleMain.java
@@ -1,23 +1,25 @@
 package Jobsheet3;
+import java.util.Scanner;
 
 public class RectangleMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Rectangle[] rectangleArray = new Rectangle[3];
         
-        rectangleArray[0] = new Rectangle();
-        rectangleArray[0].width = 10;
-        rectangleArray[0].length = 50;
+        for (int i = 0; i < 3; i++) {
+            rectangleArray[i] = new Rectangle();
+            System.out.print("Rectangle " + i);
 
-        rectangleArray[1] = new Rectangle();
-        rectangleArray[1].width = 40;
-        rectangleArray[1].length = 120;
+            System.out.print(" Input Length: ");
+            rectangleArray[i].length = sc.nextInt();
 
-        rectangleArray[2] = new Rectangle();
-        rectangleArray[2].width = 80;
-        rectangleArray[2].length = 100;
+            System.out.print(" Input width: ");
+            rectangleArray[i].width = sc.nextInt();
+        }
 
-        System.out.println("Rectangle 1, width: " + rectangleArray[0].width + " , Length: " + rectangleArray[0].length);
-        System.out.println("Rectangle 2, width: " + rectangleArray[1].width + " , Length: " + rectangleArray[1].length);
-        System.out.println("Rectangle 3, width: " + rectangleArray[2].width + " , Length: " + rectangleArray[2].length);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Rectangle " + i);
+            System.out.println("Length: " + rectangleArray[0].length + " , Width: " + rectangleArray[0].width);
+        }
     }
 }

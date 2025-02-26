a3bbcf19804f727ccd958505e074bea74d2411ee
Exc <excellchriatian12@gmail.com>
2025-02-26 16:19:35 +0700

    Init Assignment

---
+++ Jobsheet3/Lecturer11.java
@@ -0,0 +1,16 @@
+package Jobsheet3;
+
+public class Lecturer11 {
+    String name, id;
+    int age;
+    boolean isMale;
+
+    public Lecturer11(String name, String id, int age, boolean isMale) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.isMale = isMale;
+    }
+
+    
+}
---
+++ Jobsheet3/LecturerDemo.java
@@ -0,0 +1,5 @@
+package Jobsheet3;
+
+public class LecturerDemo {
+    Lecturer11 data[] = new Lecturer11[5];
+}
